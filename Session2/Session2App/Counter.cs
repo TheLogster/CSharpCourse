d9ba262ec5e7361ed22b0ebac051150b7fcddee9
TheLogster <logie@thelogster.com>
2021-01-21 23:15:44 +0000

    Adding code from session 2

---
+++ Session2/Session2App/Counter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session2App
+{
+    internal sealed class Counter
+    {
+        public int _number;
+
+        public Counter(int number)
+        {
+            _number = number;
+        }
+
+        public void Count()
+        {
+            for (int i = 0; i < _number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Session2/Session2App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Session2App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            var c = new Counter(10);
+            c.Count();
+        }
+    }
+
+}
