06e86029ae55d978ea9a4e7c1fdb3d22f532a143
TheLogster <logie@thelogster.com>
2021-01-27 23:54:21 +0000

    Adding files from Session 3

---
+++ Session3/BaseClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3
+{
+    internal abstract class BaseClass : IDisplay
+    {
+        public string Name { get; set; }
+
+        protected Guid ID;
+
+        public BaseClass()
+        {
+            Name = "BaseClass";
+            ID = Guid.NewGuid();
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine(Name);
+        }
+}
+}
---
+++ Session3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Session3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new TestClass(99);
+            var b = t as BaseClass;
+
+            t.Data = 102;
+            t.Display();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            b.Display();
+
+            var c = TestClass.CreateNewDisplay();
+            c.Display();
+        }
+    }
+}
